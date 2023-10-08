8dbed2f6c588018bc6deffd9ca10515893e51ee4
Julian Rodriguez <Julian Rodrigez>
2023-10-07 23:08:09 -0500

    Se crea algoritmos para formula progresiva y formular central

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 
+from algorithm.formula_progresiva import FormulaProgresiva
+from algorithm.formula_central import FormulaCentral
 
-print("Hola Mundo!")
+print("Resultado fórmula Progresiva:")
+resultado_formula_progresiva = FormulaProgresiva("ln(x)", 1.8)
+resultado_formula_progresiva.imprimir_tabla()
 
-
+print("Resultado fórmula Central:")
+resultado_formula_central = FormulaCentral("ln(x)", 1.8)
+resultado_formula_central.imprimir_tabla()
