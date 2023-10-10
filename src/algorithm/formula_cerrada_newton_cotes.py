f7674b21506cbcff1b06fc845fefb89096eab5ac
Ana Isabel <anai.patino@udea.edu.co>
2023-10-09 22:25:11 -0500

    Algunos algoritmos de Ana

--- src/algorithm/formula_cerrada_newton_cotes.py
+++ src/algorithm/formula_cerrada_newton_cotes.py
@@ -1,2 +1,26 @@
 
-# Ana Isabel
+# Formulas cerradas comunes de Newton Cotes
+from sympy import *
+
+x = symbols('x')
+x0 = float(input("Ingrese el primer limite de integración: "))
+x1 = float(input("Ingrese el otro limite de integración: "))
+funcion = eval(input("Ingrese la función (ejm: x^2, pi/4): ").replace("pi", str(pi)).replace("^", "**"))
+
+ht = x1 - x0
+trapecio = (ht/2)*(funcion.subs(x,x0)+funcion.subs(x, x1)) - ht**3/12* diff(funcion, x, 2).subs(x, (x0+x1)/2)
+print("El resultado con la regla del trapecio es: " + str(trapecio))
+
+x2 = float(input("Ingrese el otro limite de integración: "))
+hs = (x2-x0)/2
+simpson = (hs/3)*(funcion.subs(x,x0)+4*(funcion.subs(x, x1))+(funcion.subs(x, x2))) - hs**5/90* diff(funcion, x, 4).subs(x, (x2+x1+x0)/3)
+
+x3 = float(input("Ingrese el otro limite de integración: "))
+hsm = (x3-x0)/3
+simpson = (3*hsm/8)*(funcion.subs(x,x0) + 3*(funcion.subs(x, x1))+3*(funcion.subs(x, x2))+ funcion.subs(x,x3))- (3* (hsm**5)/80) * diff(funcion, x, 4).subs(x, (x3+x2+x1+x0)/4)
+
+
+
+
+
+
--- src/algorithm/metodo_newton.py
+++ src/algorithm/metodo_newton.py
@@ -1,2 +1,22 @@
 
-# Ana Isabel
+# Metodo de newton o newton rapshon
+
+from sympy import *
+
+x = symbols('x')
+funcion = eval(input("Ingrese aquí la funcion: "))
+po = eval(input("Ingresa el valor inicial: ").replace("pi", str(pi)))
+tol = float(input("Ingresa el nivel de tolerancia o error: "))
+NoIteraciones = int(input("Ingresa el numero maximo de iteraciones: "))
+i = 1
+
+while i <= NoIteraciones:
+    p = po - (funcion.subs(x, po) / diff(funcion, x).subs(x, po))
+    if abs(p - po) < tol:
+        print("La raiz aproximada es: " + str(p.evalf()))
+        break    
+    i = i + 1
+    po = p    
+else:
+    print("El método de Newton-Raphson no convergió después de", NoIteraciones, "iteraciones.")
+
